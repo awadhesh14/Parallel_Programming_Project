9d1f20fa001f1c858e9d058b234fa8a1679a7c60
awadhesh14 <dapst8@gmail.com>
2019-04-04 14:37:37 +0530

    wss

--- main.cu
+++ main.cu
@@ -1,2 +1,4 @@
 ##include <bits/stdc++.h>
 //sorting in thrust https://stackoverflow.com/questions/23541503/sorting-arrays-of-structures-in-cuda/23645954
+//Merge SearchSet http://on-demand.gputechconf.com/gtc/2013/presentations/S3414-Efficient-Merge-Search-Set-Operations.pdf
+//thrust SET https://thrust.github.io/doc/group__set__operations.html
