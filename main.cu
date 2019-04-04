388d5ad1b562c2016f25da6748e3bcd92dda2ef8
awadhesh14 <dapst8@gmail.com>
2019-04-05 00:17:47 +0530

    added serial vldb

--- main.cu
+++ main.cu
@@ -2,4 +2,3 @@
 //sorting in thrust https://stackoverflow.com/questions/23541503/sorting-arrays-of-structures-in-cuda/23645954
 //Merge SearchSet http://on-demand.gputechconf.com/gtc/2013/presentations/S3414-Efficient-Merge-Search-Set-Operations.pdf
 //thrust SET https://thrust.github.io/doc/group__set__operations.html
-vfvfvfvfffvfvv
